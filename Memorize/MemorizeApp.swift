7d8594aeb7fd3f6738828f107071feb0b5715ad9
0x1uke <luke@0x1uke.com>
2023-11-17 18:39:11 -0500

    Initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Luke on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
